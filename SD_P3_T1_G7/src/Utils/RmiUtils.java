9579d619463dd6906193a6afd6dc83ba9a1a5f3a
fmteixeira <fmteixeira@ua.pt>
2014-06-04 15:29:26 +0000

    Removed id inconsistencies and fixed attempts mechanism

--- SD_P3_T1_G7/src/Utils/RmiUtils.java
+++ SD_P3_T1_G7/src/Utils/RmiUtils.java
@@ -5,24 +5,22 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class RmiUtils {
-	static final int waitTime = 500;
-	static int remainingAttempts = 10;
+	static final int waitTime 	 = 500;
+	static final int maxAttempts = 10;
 	
-	public static final String genRepId = "genRep";
-	public static String tempStorageId = "tempStorage";
-	public static String baggageBeltConveyorId = "baggageBeltConveyor";
-	public static String arrivalTerminalId = "arrivalTerminal";
-	public static String arrivalTerminalTransferZoneId = "arrivalTerminalTransferZone";
-	public static String busId = "bus";
-	public static String baggageReclaimOfficeId = "baggageReclaimOffice";
-	public static String baggagePickupZoneId = "baggagePickupZone";
-	public static String baggageReclaimGuichetId = "baggageReclaimGuichet";
-	public static String departureTerminalEntraceZoneId = "departureTerminalEntraceId";
+	public static final String genRepId 						= "genRep";
+	public static final String tempStorageId 					= "tempStorage";
+	public static final String arrivalTerminalId 				= "arrivalTerminal";
+	public static final String arrivalTerminalTransferZoneId 	= "arrivalTerminalTransferZone";
+	public static final String busId 							= "bus";
+	public static final String baggagePickupZoneId 				= "baggagePickupZone";
+	public static final String baggageReclaimGuichetId 			= "baggageReclaimGuichet";
+	public static final String departureTerminalEntraceZoneId 	= "departureTerminalEntraceId";
 	
 	public static Registry getRMIReg( String hostname, int port, String usage ) throws RemoteException {
-    	Registry rmiReg = null;
-    	boolean registered = true;
-    	
+    	Registry rmiReg 	  = null;
+    	boolean registered 	  = true;
+    	int remainingAttempts = maxAttempts;
     	
 		do {
 			try {
