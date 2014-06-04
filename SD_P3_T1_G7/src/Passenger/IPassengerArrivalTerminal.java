f10cad63a7ca959406135809c41f3c8f1a8fbc63
fmteixeira@ua.pt <fmteixeira@ua.pt@280d6cf7-fae5-49a2-9848-19e24ef5b551>
2014-06-04 03:12:16 +0000

    Added "Throws RemoteException" to IPassengerArrivalTerminal

--- SD_P3_T1_G7/src/Passenger/IPassengerArrivalTerminal.java
+++ SD_P3_T1_G7/src/Passenger/IPassengerArrivalTerminal.java
@@ -1,4 +1,7 @@
 package Passenger;
+
+import java.rmi.RemoteException;
+
 /**
  * @author Miguel Azevedo <lobaoazevedo@ua.pt>
  * Interface para interação entre a thread de condutor (TDriver) e o terminal de chagada (MArrivalTerminal) 
@@ -9,6 +12,6 @@ public interface IPassengerArrivalTerminal {
 	 * @param passengerId
 	 * @throws InterruptedException 
 	 */
-	void whatSouldIDo(int passengerId) throws InterruptedException;
+	void whatSouldIDo(int passengerId) throws InterruptedException, RemoteException;
 
 }
