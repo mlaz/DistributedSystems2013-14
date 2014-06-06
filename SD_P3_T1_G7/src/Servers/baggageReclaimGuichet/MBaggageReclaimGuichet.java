2bb337e3ae26256e083b3029b73d636e58c75217
fmteixeira <fmteixeira@ua.pt>
2014-06-06 16:08:38 +0000

    Server Baggage Recalim uses VectorClock

--- SD_P3_T1_G7/src/Servers/baggageReclaimGuichet/MBaggageReclaimGuichet.java
+++ SD_P3_T1_G7/src/Servers/baggageReclaimGuichet/MBaggageReclaimGuichet.java
@@ -2,6 +2,8 @@ package Servers.baggageReclaimGuichet;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+import Utils.VectorClock;
+
 /**
  * @author Miguel Azevedo <lobaoazevedo@ua.pt>
  * Monitor do guichet de reclamação de bagagens 
@@ -9,22 +11,25 @@ import java.util.concurrent.locks.ReentrantLock;
 public class MBaggageReclaimGuichet implements IBaggageReclaimGuichet {
 
 	private Lock lock;
-	
+	private VectorClock vecClock;
 	/**
 	 */
-	public MBaggageReclaimGuichet() {
+	public MBaggageReclaimGuichet( int numEntities ) {
 		lock = new ReentrantLock();
+		this.vecClock = new VectorClock(numEntities);
 	}
 
 	/**
 	 * 
      * @param passengerNumber
 	 */
-	public void reclaimBags(int passengerNumber) {
+	public VectorClock reclaimBags(int passengerNumber, VectorClock extClk) {
 		lock.lock();
 		
 		try{
+			vecClock.updateClock(extClk);
 			System.out.printf("[%d] reclaiming Bags\n", passengerNumber);
+			return vecClock;
 		} finally {
 			lock.unlock();
 		}
