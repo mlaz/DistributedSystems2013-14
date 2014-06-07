50e0e17e421e0edd271c52947efd9884576fc616
fmteixeira@ua.pt <fmteixeira@ua.pt@280d6cf7-fae5-49a2-9848-19e24ef5b551>
2014-06-07 17:34:04 +0000

    Bus uses local RMI

--- SD_P3_T1_G7/src/Servers/bus/ServerBus.java
+++ SD_P3_T1_G7/src/Servers/bus/ServerBus.java
@@ -1,9 +1,12 @@
 package Servers.bus;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.rmi.AccessException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
@@ -16,38 +19,39 @@ import Utils.RmiUtils;
  */
 public class ServerBus {
 	private static final int portNumber = 22163;
-	private static final String usage = "Usage: java ServerBus [RMIRegName] [RMIRegPort]";
+	private static final String usage = "Usage: java -jar RMIBus [genRepRegistryName]";
     /**
      *
      * @param args
      */
     public static void main(String[] args) {
 		
-		if (args.length != 2) {
+		if (args.length != 1) {
 			System.out.println(usage);
 			// System.exit(1);
-			args = new String[2];
+			args = new String[1];
 			args[0] = "localhost";
-			args[1] = "22168";
 		}
 		
-		/* obter parametros do problema */
-		/* get the RMI registry */
-		Registry rmiReg = null;
+		Registry genRepRegistry = null;
 		try {
-			rmiReg = RmiUtils.getRMIReg( args[0], Integer.parseInt(args[1]), usage );
-		} catch (NumberFormatException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-		} catch (RemoteException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
+			genRepRegistry = LocateRegistry.getRegistry(args[0], RmiUtils.rmiPort);
+		} catch( RemoteException e ) {
+			System.err.println( "Error accessing the RMI registry: " + e.getMessage() );
+			e.printStackTrace();
+			System.exit(1);
 		}
-		System.out.println("RMI registry located");
+		System.out.println( "GenRep RMI registry accessed" );
 		
 		IGenRep genRep = null;
+		int busInterval = 0;
+		int numSeats 	= 0;
+		int numEntities = 0;
 		try {
-			genRep = (IGenRep) rmiReg.lookup(RmiUtils.genRepId);
+			genRep = (IGenRep) genRepRegistry.lookup(RmiUtils.genRepId);
+			busInterval = genRep.getBusWaitTime();
+			numSeats  	= genRep.getNumBusSeats();
+			numEntities = genRep.getNumPassengers() + 2;
 		} catch (AccessException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -64,19 +68,6 @@ public class ServerBus {
 		
 		System.out.println("GenRep accessed");
 		
-		int busInterval = 0;
-		int numSeats 	= 0;
-		int numEntities = 0;
-		try {
-			busInterval = genRep.getBusWaitTime();
-			numSeats  	= genRep.getNumBusSeats();
-			numEntities = genRep.getNumPassengers() + 2;
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.exit(1);
-		}
-		
 		/* establecer o serviço */
 		MBus bus = new MBus(numSeats, busInterval, genRep, numEntities);
 		IBus busInter = null;
@@ -91,8 +82,18 @@ public class ServerBus {
 		
 		System.out.println("Bus stub created");
 		
+		Registry registry = null;
 		try {
-			genRep.bind(RmiUtils.busId, busInter);
+			registry = LocateRegistry.getRegistry(RmiUtils.rmiPort);
+		} catch( RemoteException e ) {
+			System.err.println( "Error accessing the RMI registry: " + e.getMessage() );
+			e.printStackTrace();
+			System.exit(1);
+		}
+		System.out.println( "Local RMI registry accessed" );
+		
+		try {
+			registry.bind(RmiUtils.busId, busInter);
 		} catch (RemoteException | AlreadyBoundException e) {
 			System.err.println("Error binding the Bus to the RMI registry");
 			e.printStackTrace();
@@ -100,6 +101,13 @@ public class ServerBus {
 		}
 		
         System.out.println("Bus binded to RMI registry (port " + portNumber + ")");
+
+        try {
+			genRep.registerService(RmiUtils.arrivalTerminalId, InetAddress.getLocalHost().getHostName(), portNumber);
+		} catch (RemoteException | UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         System.out.println("Ready");
     }
 }
