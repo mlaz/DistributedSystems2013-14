e0b83d085a179feda3c310404e32560417ded234
fmteixeira <fmteixeira@ua.pt>
2014-06-07 17:18:54 +0000

    Change TempStorage to local RMI

--- SD_P3_T1_G7/src/Servers/tempStorage/ServerTempBaggaStorage.java
+++ SD_P3_T1_G7/src/Servers/tempStorage/ServerTempBaggaStorage.java
@@ -1,8 +1,12 @@
 package Servers.tempStorage;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.rmi.AccessException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
@@ -14,51 +18,54 @@ import Utils.RmiUtils;
  */
 public class ServerTempBaggaStorage {
 	private static final int portNumber = 22167;
-	private static final String usage 	= "Usage: java ServerTempBaggageStorage [RMIRegName] [RMIRegPort]";
+	private static final String usage 	= "Usage: java -jar RMITemporaryBaggageStorage [genRepRegistryName]";
     /**
      *
      * @param args
      */
     public static void main(String[] args) {
 		
-		if (args.length != 2) {
+		if (args.length != 1) {
 			System.out.println( usage );
 			// System.exit(1);
-			args = new String[2];
+			args = new String[1];
 			args[0] = "localhost";
-			args[1] = "22168";
 		}
 		
-		/* get the RMI registry */
-		Registry rmiReg = null;
+
+		Registry genRepRegistry = null;
 		try {
-			rmiReg = RmiUtils.getRMIReg( args[0], Integer.parseInt(args[1]), usage );
-		} catch (NumberFormatException e1) {
-			System.err.println("The second argument isn't a valid port number");
+			genRepRegistry = LocateRegistry.getRegistry(args[0], RmiUtils.rmiPort);
+		} catch( RemoteException e ) {
+			System.err.println( "Error accessing the RMI registry: " + e.getMessage() );
+			e.printStackTrace();
+			System.exit(1);
+		}
+		System.out.println( "GenRep RMI registry accessed" );
+		
+		IGenRep genRep = null;
+		int numEntities = 0;
+		try {
+			genRep = (IGenRep) genRepRegistry.lookup(RmiUtils.genRepId);
+			numEntities = genRep.getNumPassengers() + 2;
+		} catch (AccessException e1) {
+			// TODO Auto-generated catch block
 			e1.printStackTrace();
 			System.exit(1);
 		} catch (RemoteException e1) {
-			System.err.println("The RMI registry is unavailable");
+			// TODO Auto-generated catch block
 			e1.printStackTrace();
 			System.exit(1);
-		}
-		
-		System.out.println("RMI registry located");
-		
-		int numEntities = 0;
-		IGenRep genRep = null;
-		try {
-			genRep = (IGenRep) rmiReg.lookup(RmiUtils.genRepId);
-			numEntities = genRep.getNumPassengers() + 2;
-		} catch ( RemoteException | NotBoundException e1) {
+		} catch (NotBoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 			System.exit(1);
 		}
 		
+		System.out.println("GenRep accessed");
+		
 		/* establecer o serviço */
 		MTempBaggageStorage tempStorage = new MTempBaggageStorage(numEntities);
-		
 		ITempStorage tempStorageInter   = null;
 		
 		try {
@@ -71,8 +78,19 @@ public class ServerTempBaggaStorage {
 		
 		System.out.println("TempStorage stub created");
 		
+		Registry registry = null;
 		try {
-			genRep.bind(RmiUtils.tempStorageId, tempStorageInter);
+			registry = LocateRegistry.getRegistry(RmiUtils.rmiPort);
+		} catch( RemoteException e ) {
+			System.err.println( "Error accessing the RMI registry: " + e.getMessage() );
+			e.printStackTrace();
+			System.exit(1);
+		}
+		System.out.println( "Local RMI registry accessed" );
+		
+		
+		try {
+			registry.bind(RmiUtils.tempStorageId, tempStorageInter);
 		} catch (RemoteException | AlreadyBoundException e) {
 			System.err.println("Error binding the TempStorage to the RMI registry");
 			e.printStackTrace();
@@ -80,6 +98,14 @@ public class ServerTempBaggaStorage {
 		}
 		
 		System.out.println("Temporary Baggage Storage binded to RMI registry (port "+portNumber+")");
+
+		try {
+			genRep.registerService(RmiUtils.arrivalTerminalId, InetAddress.getLocalHost().getHostName(), portNumber);
+		} catch (RemoteException | UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		System.out.println("Ready");
 	}
 }
