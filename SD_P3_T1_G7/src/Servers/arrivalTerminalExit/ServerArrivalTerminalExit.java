473202171eb7e741f4d95e5b97d658653495bfac
fmteixeira <fmteixeira@ua.pt>
2014-06-07 22:12:10 +0000

    Corrections ArrTermExit

--- SD_P3_T1_G7/src/Servers/arrivalTerminalExit/ServerArrivalTerminalExit.java
+++ SD_P3_T1_G7/src/Servers/arrivalTerminalExit/ServerArrivalTerminalExit.java
@@ -28,12 +28,11 @@ public class ServerArrivalTerminalExit {
      */
     public static void main(String[] args) {
 
-		if (args.length != 2) {
+		if (args.length != 1) {
 			System.out.println(usage);
 			// System.exit(1);
-			args = new String[2];
+			args = new String[1];
 			args[0] = "localhost";
-			args[1] = "22168";
 		}
 
 		Registry genRepRegistry = null;
