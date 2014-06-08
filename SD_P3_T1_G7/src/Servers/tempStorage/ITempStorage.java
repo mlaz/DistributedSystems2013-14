ff4052fd3cd5372f6254ad6f217e64c664705326
fmteixeira@ua.pt <fmteixeira@ua.pt@280d6cf7-fae5-49a2-9848-19e24ef5b551>
2014-06-08 17:37:08 +0000

    JavaDoc of tempStorage package done

--- SD_P3_T1_G7/src/Servers/tempStorage/ITempStorage.java
+++ SD_P3_T1_G7/src/Servers/tempStorage/ITempStorage.java
@@ -4,6 +4,12 @@ import java.rmi.Remote;
 
 import Porter.IPorterTempBaggageStorage;
 
+/**
+ * This interface contains all the methods that MTempBaggageStorage must implement.
+ * 
+ * @author Filipe Teixeira <fmteixeira@ua.pt>
+ * @author Miguel Azevedo <lobaoazevedo@ua.pt>
+ */
 public interface ITempStorage extends IPorterTempBaggageStorage, Remote {
 
 }
