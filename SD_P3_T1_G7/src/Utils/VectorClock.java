35809efa0bbac25a9dc7fdd8956bf4093b46486a
fmteixeira@ua.pt <fmteixeira@ua.pt@280d6cf7-fae5-49a2-9848-19e24ef5b551>
2014-06-08 14:54:19 +0000

    JavaDoc of Utils package done

--- SD_P3_T1_G7/src/Utils/VectorClock.java
+++ SD_P3_T1_G7/src/Utils/VectorClock.java
@@ -2,30 +2,60 @@ package Utils;
 
 import java.io.Serializable;
 
+/**
+ * Implements a VectorialClock
+ * 
+ * @author Filipe Teixeira <fmteixeira@ua.pt>
+ * @author Miguel Azevedo <lobaoazevedo@ua.pt>
+ */
+
 public class VectorClock implements Comparable<VectorClock>, Serializable {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 3963949565814453628L;
 	
+	/**
+	 * Stores the vectorial clock.
+	 */
 	private int[] clock;
 	
+	/**
+	 * Instanciates a VectorClock object.
+	 * 
+	 * @param size Number of entities that use the clock.
+	 */
 	public VectorClock(int size) {
 		this.clock = new int[size];
 	}
 	
+	
+	/**
+	 * Increments one position of the VectorClock.
+	 * @param id The position to be incremented.
+	 * @throws ArrayIndexOutOfBoundsException Thrown if id is negative or larger than size.
+	 */
 	public void increment(int id) throws ArrayIndexOutOfBoundsException {
 		clock[id]++;
 	}
 	
+	/**
+	 * Returns an array of integers with the values of the clock.
+	 * @return An array of integers with the values of the clock.
+	 */
 	public int[] getClock() {
 		return clock;
 	}
 	
+	/**
+	 * Returns the size of the clock
+	 * @return The size of the clock
+	 */
 	public int getClockSize() {
 		return clock.length;
 	}
 	
+	/**
+	 * Updates this Clock with the information of an external clock.
+	 * @param external Clock used to update this clock
+	 */
 	public void updateClock(VectorClock external) {
 		if( this.getClockSize() != external.getClockSize() )
 			throw new IllegalArgumentException();
@@ -46,6 +76,11 @@ public class VectorClock implements Comparable<VectorClock>, Serializable {
 		return sumThis - sumOther;
 	}
 	
+	/**
+	 * Sums an integer array
+	 * @param array Array to be summed
+	 * @return The sum of the array
+	 */
 	private int sum(int[] array) {
 		int sum = 0;
 		for(int i:array) {
