eea77a2c5c5de616d269ce1243b5d80feaa20637
fmteixeira@ua.pt <fmteixeira@ua.pt@280d6cf7-fae5-49a2-9848-19e24ef5b551>
2014-06-08 18:21:51 +0000

    JavaDoc of baggagePickupZone package done

--- SD_P3_T1_G7/src/Servers/baggagePickupZone/IBaggagePickupZone.java
+++ SD_P3_T1_G7/src/Servers/baggagePickupZone/IBaggagePickupZone.java
@@ -5,6 +5,11 @@ import java.rmi.Remote;
 import Passenger.IPassengerBaggageCollectionPoint;
 import Porter.IPorterBaggagePickupZone;
 
+/**
+ * This interface contains all the methods that MBaggagePickupZone must implement.
+ * @author Filipe Teixeira <fmteixeira@ua.pt>
+ * @author Miguel Azevedo <lobaoazevedo@ua.pt>
+ */
 public interface IBaggagePickupZone extends IPorterBaggagePickupZone, IPassengerBaggageCollectionPoint, Remote {
 
 }
---
+++ SD_P3_T1_G7/src/Servers/baggagePickupZone/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Implements the Baggage Pickup Zone with RMI communications <p> 
+ */
+
+package Servers.baggagePickupZone;
