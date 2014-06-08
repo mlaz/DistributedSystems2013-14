5ed81977abba31ee26dab8d822164185eee05503
fmteixeira@ua.pt <fmteixeira@ua.pt@280d6cf7-fae5-49a2-9848-19e24ef5b551>
2014-06-08 18:12:14 +0000

    JavaDoc of baggageReclaim package done

--- SD_P3_T1_G7/src/Servers/baggageReclaimGuichet/IBaggageReclaimGuichet.java
+++ SD_P3_T1_G7/src/Servers/baggageReclaimGuichet/IBaggageReclaimGuichet.java
@@ -4,6 +4,12 @@ import java.rmi.Remote;
 
 import Passenger.IPassengerBaggageReclaimGuichet;
 
+/**
+ * This interface contains all the methods that MBaggageReclaimGuichet must implement.
+ * 
+ * @author Filipe Teixeira <fmteixeira@ua.pt>
+ * @author Miguel Azevedo <lobaoazevedo@ua.pt>
+ */
 public interface IBaggageReclaimGuichet extends IPassengerBaggageReclaimGuichet, Remote {
 
 }
--- SD_P3_T1_G7/src/Servers/baggageReclaimGuichet/MBaggageReclaimGuichet.java
+++ SD_P3_T1_G7/src/Servers/baggageReclaimGuichet/MBaggageReclaimGuichet.java
@@ -5,24 +5,31 @@ import java.util.concurrent.locks.ReentrantLock;
 import Utils.VectorClock;
 
 /**
- * @author Miguel Azevedo <lobaoazevedo@ua.pt>
- * Monitor do guichet de reclamação de bagagens 
+ * Class that implements the Baggage Reclaim Guichet server services.
+ * @author Filipe Teixeira <fmteixeira@ua.pt>
+ * @author Miguel Azevedo <lobaoazevedo@ua.pt> 
  */
 public class MBaggageReclaimGuichet implements IBaggageReclaimGuichet {
 
-	private Lock lock;
-	private VectorClock vecClock;
 	/**
+	 *	The lock 
+	 */
+	private Lock lock;
+	/**
+	 * The clock
+	 */
+	private VectorClock vecClock;
+	
+	
+	/**
+	 * Instanciates a Baggage Reclaim Guichet object
+	 * @param numEntities The number of entities that will use the VectorClock at the same time
 	 */
 	public MBaggageReclaimGuichet( int numEntities ) {
 		lock = new ReentrantLock();
 		this.vecClock = new VectorClock(numEntities);
 	}
 
-	/**
-	 * 
-     * @param passengerNumber
-	 */
 	public VectorClock reclaimBags(int passengerNumber, VectorClock extClk) {
 		lock.lock();
 		
